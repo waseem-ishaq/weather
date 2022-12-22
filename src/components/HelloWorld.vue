0e403917b50629bc6ae95ed531958f53548a90bc
waseem-ishaq <wkinglastx@gmail.com>
2022-12-22 16:44:17 +0500

    ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,104 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <div class="width:50% mx-auto pa-4">
+      <div class="d-flex">
+        <div class="search-filed">
+          <v-text-field
+          v-model="city"  
+          class="rounded-pill"
+            outlined
+            label="Enter city name here.."
+            type="text"
+            @change="fetchWeatherApi()"
+          />
+        </div>
+        <div>
+          <v-btn @click="fetchWeatherApi()" class="pa-6 rounded-pill info search-btn">
+            <h3>search</h3>
+          </v-btn>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div>
+        <div class="d-flex">
+          <div>
+            <h1>{{ this.city }},</h1>
+          </div>
+          <div>
+            <h1>{{ this.country }}</h1>
+          </div>
+        </div>
+        <div>
+          <b>{{ this.wCondition }} </b>
+        </div>
+        <div>{{ this.tem }}C</div>
+        <div><b>Humidity :</b>{{ this.humidity }}%</div>
+      </div>
+      <div>
+        <img class="img" v-bind:src="this.imgsrc" />
+      </div>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+
+  data() {
+    return {
+      city: "",
+      country: "",
+      wCondition: "",
+      tem: "",
+      imgsrc: "",
+      humidity: "",
+    };
+  },
+  methods: {
+    fetchWeatherApi() {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=0ca12bf69c2c9bb5091f9d18f3423b19`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.country = data.sys.country;
+          this.wCondition = data.weather[0].description;
+          this.tem = data.main.temp;
+          this.imgsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        });
+    },
+    fetchCityesApi() {
+      fetch("https://countriesnow.space/api/v0.1/countries/population/cities")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.data);
+          this.city = data.data;
+        });
+    },
+  },
+  mounted() {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=lahore&appid=0ca12bf69c2c9bb5091f9d18f3423b19`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.city=data.name;
+        this.country = data.sys.country;
+        this.wCondition = data.weather[0].description;
+        this.tem = Math.round(data.main.temp - 373);
+        this.imgsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        this.humidity = data.main.humidity;
+      });
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.search-filed {
+  width: 100%;
 }
 </style>
